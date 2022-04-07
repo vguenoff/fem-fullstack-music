34bbee4ae700acbefc53a0f6f963d6e3b4bc2201
vguenoff <vguenoff@gmail.com>
2022-04-07 09:42:21 +0300

    PlayerLayout

---
+++ components/PlayerLayout.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/layout'
+
+export const PlayerLayout = ({ children }) => {
+    return (
+        <Box>
+            layout
+            {children}
+        </Box>
+    )
+}
