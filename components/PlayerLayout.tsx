cfb8c5095b6d7cb2ba6ab4ffac12ca013f1f099f
vguenoff <vguenoff@gmail.com>
2022-04-08 09:53:39 +0300

    Layout wip

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,10 +1,18 @@
 import { Box } from '@chakra-ui/layout'
+import { Sidebar } from 'components/Sidebar'
 
 export const PlayerLayout = ({ children }) => {
     return (
-        <Box>
-            layout
-            {children}
+        <Box width="100vw" height="100vh">
+            <Box position="absolute" top={0} width="250px" left={0}>
+                <Sidebar />
+            </Box>
+            <Box marginLeft={250} marginBottom={100}>
+                {children}
+            </Box>
+            <Box position="absolute" left={0} border={0}>
+                player
+            </Box>
         </Box>
     )
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import {
+    Box,
+    List,
+    ListItem,
+    ListIcon,
+    Divider,
+    Center,
+    LinkBox,
+    LinkOverlay,
+} from '@chakra-ui/layout'
+
+import {
+    MdHome,
+    MdSearch,
+    MdLibraryMusic,
+    MdPlaylistAdd,
+    MdFavorite,
+} from 'react-icons/md'
+
+export const Sidebar = () => {
+    return (
+        <>
+            <div>Hello from sidebatr</div>
+        </>
+    )
+}
