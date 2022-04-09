5c51189f34c17b27515614d18f9e1d3d9c4e4f37
vguenoff <vguenoff@gmail.com>
2022-04-09 17:09:15 +0300

    navMenu

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,23 +1,42 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import {
     Box,
-    // List,
-    // ListItem,
-    // ListIcon,
-    // Divider,
-    // Center,
-    // LinkBox,
-    // LinkOverlay,
+    List,
+    ListItem,
+    ListIcon,
+    Divider,
+    Center,
+    LinkBox,
+    LinkOverlay,
 } from '@chakra-ui/layout'
 
-// import {
-//     MdHome,
-//     MdSearch,
-//     MdLibraryMusic,
-//     MdPlaylistAdd,
-//     MdFavorite,
-// } from 'react-icons/md'
+import {
+    MdHome,
+    MdSearch,
+    MdLibraryMusic,
+    MdPlaylistAdd,
+    MdFavorite,
+} from 'react-icons/md'
+
+const navMenu = [
+    {
+        name: 'Home',
+        icon: MdHome,
+        route: '/',
+    },
+    {
+        name: 'Search',
+        icon: MdSearch,
+        route: '/search',
+    },
+    {
+        name: 'Your Library',
+        icon: MdLibraryMusic,
+        route: '/library',
+    },
+]
 
 export const Sidebar = () => {
     return (
@@ -32,6 +51,30 @@ export const Sidebar = () => {
                 <Box width="120px" marginBottom="20px" paddingX="20px">
                     <Image src="/logo.svg" height={60} width={120} />
                 </Box>
+            </Box>
+            <Box marginBottom="20px">
+                <List spacing={2}>
+                    {navMenu.map((menu, i) => (
+                        <ListItem
+                            paddingX="20px"
+                            fontSize="16px"
+                            key={`${menu.name + i}`}
+                        >
+                            <LinkBox>
+                                <Link href={menu.route} passHref>
+                                    <LinkOverlay>
+                                        <ListIcon
+                                            as={menu.icon}
+                                            color="white"
+                                            marginRight="10px"
+                                        />
+                                        {menu.name}
+                                    </LinkOverlay>
+                                </Link>
+                            </LinkBox>
+                        </ListItem>
+                    ))}
+                </List>
             </Box>
         </Box>
     )
